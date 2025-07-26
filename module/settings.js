af4439ad0d23d9ac9945e31e2dfb524021581abf
Mykhailo Budiakov <mishabudjakov@gmail.com>
2025-07-26 12:36:50 +0300

    add onlyHandlerHint to menus

--- module/settings.js
+++ module/settings.js
@@ -131,7 +131,7 @@ export default function registerSystemSettings() {
   game.settings.registerMenu(DG.ID, "automation", {
     name: game.i18n.localize(`DG.SettingsMenu.automation.name`),
     label: game.i18n.localize(`DG.SettingsMenu.automation.label`),
-    hint: "",
+    hint: game.i18n.localize(`DG.SettingsMenu.onlyHandlerHint`),
     icon: "fa-solid fa-dice",
     type: AutomationSettings,
     restricted: true
@@ -141,7 +141,7 @@ export default function registerSystemSettings() {
   game.settings.registerMenu(DG.ID, "handler", {
     name: game.i18n.localize(`DG.SettingsMenu.handler.name`),
     label: game.i18n.localize(`DG.SettingsMenu.handler.label`),
-    hint: "",
+    hint: game.i18n.localize(`DG.SettingsMenu.onlyHandlerHint`),
     icon: "fa-solid fa-dice",
     type: HandlerSettings,
     restricted: true
